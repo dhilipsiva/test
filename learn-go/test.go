f802d02f4c0cb387c2c4a3f076b8684e83624c49
dhilipsiva <dhilipsiva@gmail.com>
2016-01-26 15:26:34 +0530

    Test Go Script

---
+++ learn-go/test.go
@@ -0,0 +1,19 @@
+//
+// test.go
+// Copyright (C) 2016 dhilipsiva <dhilipsiva@gmail.com>
+//
+// Distributed under terms of the MIT license.
+//
+
+package main
+
+import "fmt"
+
+func foo() {
+	fmt.Println("Foo!")
+}
+
+func main() {
+	fmt.Println("Wow!")
+	foo()
+}
