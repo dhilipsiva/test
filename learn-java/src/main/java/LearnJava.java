ac5b7d2e011a6642aeddb8d32aeb892b8a976365
dhilipsiva <dhilipsiva@gmail.com>
2016-04-09 20:38:08 +0530

    Using Idea plugin

---
+++ learn-java/src/main/java/LearnJava.java
@@ -0,0 +1,13 @@
+/*
+ * LearnJava.java
+ * Copyright (C) 2016 dhilipsiva <dhilipsiva@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+public class LearnJava
+{
+	public LearnJava() {
+	}
+}
+
