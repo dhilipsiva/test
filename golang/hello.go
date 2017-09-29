ab8b30ab309f767bb77d37ff02450b2b74740f9e
dhilipsiva <dhilipsiva@gmail.com>
2017-09-29 15:09:52 +0530

    Experimenting with Go

---
+++ golang/hello.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func add(num1 int, num2 int) int {
+	return num1 + num2
+}
+
+type Point struct {
+	x int
+	y int
+}
+
+func main() {
+	/*
+		fmt.Println("Hello World", add(12, 13))
+		for i := 0; i < 10; i++ {
+			fmt.Println(i)
+		}
+	*/
+	/*
+		var x int = 5
+		fmt.Println(x)
+		p := &x
+		fmt.Println(*p)
+		*p = 9
+		fmt.Println(x, *p)
+	*/
+	// p * Point = Point{5, 4}
+	p := Point{y: 5, x: 4}
+	fmt.Println(p.x)
+}
