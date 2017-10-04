2a78de6fa2e729d2a7dafab9b6babd8591d307ed
dhilipsiva <dhilipsiva@gmail.com>
2017-10-04 15:49:41 +0530

    Just some methods

--- golang/methods.go
+++ golang/methods.go
@@ -5,15 +5,9 @@ import (
 	"math"
 )
 
-type Foo struct {
-	bar float64
-	baz float64
-}
-
 type Vertex struct {
-	X   float64
-	Y   float64
-	foo Foo
+	X float64
+	Y float64
 }
 
 func (v Vertex) Abs() float64 {
@@ -23,16 +17,11 @@ func (v Vertex) Abs() float64 {
 func (v *Vertex) Scale(i int) {
 	v.X = v.X * float64(i)
 	v.Y = v.Y * float64(i)
-	v.foo.bar = v.foo.bar * float64(i)
-	v.foo.baz = v.foo.baz * float64(i)
 }
 
 func main() {
-	foo := Foo{3, 4}
-	v := Vertex{X: 3, Y: 5, foo: foo}
+	v := Vertex{X: 3, Y: 5}
 	fmt.Println(v.Abs())
-	fmt.Println(v.foo)
 	v.Scale(10)
 	fmt.Println(v.Abs())
-	fmt.Println(v.foo)
 }
